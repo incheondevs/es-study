4306b256be014a7283c425c1634a69adc0d20495
RbertKo <myeongsku@gmail.com>
2023-01-18 04:21:16 +0900

    :tada: 프로젝트 생성

---
+++ myeongseok/web-app-for-es/src/main/kotlin/com/teckovelop/fores/ForEsApplication.kt
@@ -0,0 +1,11 @@
+package com.teckovelop.fores
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForEsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForEsApplication>(*args)
+}
---
+++ myeongseok/web-app-for-es/src/test/kotlin/com/teckovelop/fores/ForEsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teckovelop.fores
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForEsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
